1dd523697afdab089f325f9eae76120ce98aaa62
kristapsv <kristaps.vitols@printful.com>
2019-05-05 19:50:44 +0300

    init nuxt docker

---
+++ client/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <dvi>
+        <div>API fetch....</div>
+        <p v-for="text in texts">{{ text }}</p>
+    </dvi>
+</template>
+<script>
+    import Logo from '~/components/Logo.vue'
+
+    export default {
+        components: {
+            Logo
+        },
+        created() {
+            this.getTexts();
+        },
+        data() {
+            return {
+                texts: [],
+            }
+        },
+        methods: {
+            getTexts() {
+                fetch('/api/texts')
+                    .then(re => re.json())
+                    .then(data => this.texts = data.texts);
+            },
+        }
+    }
+</script>
